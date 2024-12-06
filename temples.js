597778a233e6ec24813d4831169fed6526a6341d
bienvenuemabio <kou22008@byui.edu>
2024-12-06 06:03:37 +0000

    added some code

--- temples.js
+++ temples.js
@@ -3,3 +3,12 @@ const today= new Date();
 currentyear.textContent = today.getFullYear();
 const modifdat = document.getElementById('dat');
 modifdat.textContent = today;
+
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
