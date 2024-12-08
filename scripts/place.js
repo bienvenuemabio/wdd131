104b069a27cce7cdb172ee86471f350f9e188f76
bienvenuemabio <kou22008@byui.edu>
2024-12-08 05:13:18 +0000

    added images

---
+++ scripts/place.js
@@ -0,0 +1,7 @@
+const currentyear = document.getElementById('foot');
+const today= new Date();
+currentyear.textContent = today.getFullYear();
+const modifdat = document.getElementById('dat');
+modifdat.textContent = today;
+
+// function calculatewindchill
