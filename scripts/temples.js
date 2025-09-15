93dc3ff1b8716d83ef017880fb0ff800273e32be
bienvenuemabio <kou22008@byui.edu>
2025-09-15 02:30:30 +0000

    add some js code

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,3 +4,11 @@ year.textContent = new Date().getFullYear();
 const lastmodified = document.querySelector("#lastModified");
 alert(document.lastModified);
 lastmodified.textContent = new Date(document.lastModified);
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
