5adb4d0537b447666a9dffcab8477fe1a551e38a
bienvenuemabio <kou22008@byui.edu>
2025-10-15 01:17:36 +0000

    addded code

--- project/scripts/msaveur.js
+++ project/scripts/msaveur.js
@@ -3,3 +3,11 @@ year.textContent = new Date().getFullYear();
 
 const last = document.querySelector("#last");
 last.textContent = new Date();
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
